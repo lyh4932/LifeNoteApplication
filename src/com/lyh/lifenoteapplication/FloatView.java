72f95d433e783e8fbdd98f027e202463ae83c208
luoyh1023 <luoyh1023@thundersoft.com>
2015-03-27 18:30:22 +0800

    float icon test

--- src/com/lyh/lifenoteapplication/FloatView.java
+++ src/com/lyh/lifenoteapplication/FloatView.java
@@ -1,52 +1,77 @@
 package com.lyh.lifenoteapplication;
 
 import android.content.Context;
+
+import android.graphics.Rect;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class FloatView extends ImageView {
-    private float startX; // the first pointer index of x coordinate
-    private float startY; // the first pointer index of Y coordinate
-    private float x;// the coordinate of X
-    private float y;// the coordinate of Y
-
-    private WindowManager wm = (WindowManager) getContext().getApplicationContext()
-            .getSystemService(getContext().WINDOW_SERVICE);
-    private WindowManager.LayoutParams wmlp = ((MyApplication) getContext().getApplicationContext())
-            .getLayoutParams();
+    private float mTouchX;
+    private float mTouchY;
+    private float x;
+    private float y;
+    private float mStartX;
+    private float mStartY;
+    private OnClickListener mClickListener;
+    private WindowManager windowManager = (WindowManager) getContext().getApplicationContext()
+            .getSystemService(Context.WINDOW_SERVICE);
+    // 此windowManagerParams变量为获取的全局变量，用以保存悬浮窗口的属性
+    private WindowManager.LayoutParams windowManagerParams = ((FloatApplication) getContext()
+            .getApplicationContext()).getWindowParams();
 
     public FloatView(Context context) {
         super(context);
-        // TODO Auto-generated constructor stub
+
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        // To get the coordinate on the screen
+        // 获取到状态栏的高度
+        Rect frame = new Rect();
+        getWindowVisibleDisplayFrame(frame);
+        int statusBarHeight = frame.top;
+        System.out.println("statusBarHeight:" + statusBarHeight);
+        // 获取相对屏幕的坐标，即以屏幕左上角为原点
         x = event.getRawX();
-        y = event.getRawY() - 25; // 25 the height of the status bar
+        y = event.getRawY() - statusBarHeight; // statusBarHeight是系统状态栏的高度
+        Log.i("tag", "currX" + x + "====currY" + y);
         switch (event.getAction()) {
-        // begin to move the view
-        case MotionEvent.ACTION_DOWN:
-            startX = event.getX();
-            startY = event.getY();
+        case MotionEvent.ACTION_DOWN: // 捕获手指触摸按下动作
+            // 获取相对View的坐标，即以此View左上角为原点
+            mTouchX = event.getX();
+            mTouchY = event.getY();
+            mStartX = x;
+            mStartY = y;
+            Log.i("tag", "startX" + mTouchX + "====startY" + mTouchY);
             break;
-        case MotionEvent.ACTION_MOVE:
+        case MotionEvent.ACTION_MOVE: // 捕获手指触摸移动动作
             updateViewPosition();
             break;
-        case MotionEvent.ACTION_UP:
+        case MotionEvent.ACTION_UP: // 捕获手指触摸离开动作
             updateViewPosition();
-            startX = startY = 0;
+            mTouchX = mTouchY = 0;
+            if ((x - mStartX) < 5 && (y - mStartY) < 5) {
+                if (mClickListener != null) {
+                    mClickListener.onClick(this);
+                }
+            }
             break;
         }
         return true;
     }
 
+    @Override
+    public void setOnClickListener(OnClickListener l) {
+        this.mClickListener = l;
+    }
+
     private void updateViewPosition() {
-        // To change the position of the float view
-        wmlp.x = (int) (x - startX);
-        wmlp.y = (int) (y - startY);
-        wm.updateViewLayout(this, wmlp);
+        // 更新浮动窗口位置参数
+        windowManagerParams.x = (int) (x - mTouchX);
+        windowManagerParams.y = (int) (y - mTouchY);
+        windowManager.updateViewLayout(this, windowManagerParams); // 刷新显示
     }
 }
